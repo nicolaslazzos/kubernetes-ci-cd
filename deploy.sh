c467c43b973b248b2284931619c584e96e057ba8
nicolaslazzos <nicolaslazzos09@gmail.com>
2022-07-21 00:22:50 -0300

    adding travis deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# we are tagging the images with :latest to be sure the :latest is always the latest version, and with a :SHA to be able to correclty update the images version on production
+docker build -t nicolaslazzos/multiple-docker-client:latest -t nicolaslazzos/multiple-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nicolaslazzos/multiple-docker-server:latest -t nicolaslazzos/multiple-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nicolaslazzos/multiple-docker-worker:latest -t nicolaslazzos/multiple-docker-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# push all the images to docker hub
+docker push nicolaslazzos/multiple-docker-client:latest
+docker push nicolaslazzos/multiple-docker-server:latest
+docker push nicolaslazzos/multiple-docker-worker:latest
+docker push nicolaslazzos/multiple-docker-client:$SHA
+docker push nicolaslazzos/multiple-docker-server:$SHA
+docker push nicolaslazzos/multiple-docker-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment server=nicolaslazzos/multiple-docker-client:$SHA
+kubectl set image deployments/server-deployment server=nicolaslazzos/multiple-docker-server:$SHA
+kubectl set image deployments/worker-deployment server=nicolaslazzos/multiple-docker-worker:$SHA
